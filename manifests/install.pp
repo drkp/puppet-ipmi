5984007d7ea14620ca09174bb4389de24f5ec6ee
Dan R. K. Ports <drkp@cs.washington.edu>
2016-08-12 23:59:57 -0700

    Patch Default-Start/Stop lines in init.d to work with systemd

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,4 +6,18 @@ class ipmi::install {
   package { $ipmi::params::ipmi_package:
     ensure => present,
   }
+  ->
+  file_line { 'init_default_start':
+    ensure => present,
+    path   => '/etc/init.d/openipmi',
+    line   => '# Default-Start: 2 3 4 5',
+    match  => '^# Default-Start',
+  }
+  ->
+  file_line { 'init_default_stop':
+    ensure => present,
+    path   => '/etc/init.d/openipmi',
+    line   => '# Default-Stop: 0 1 6',
+    match  => '^# Default-Stop',
+  }
 }
